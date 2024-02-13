d0de18e8eae27e4fd961efd6a440f6a6fbfc6c33
ajith1506 <ajithkumararun1111@gmail.com>
2024-02-13 16:56:35 +0530

    changes

--- models/mechanicModel.js
+++ models/mechanicModel.js
@@ -7,38 +7,30 @@ const mechanicSchema = new mongoose.Schema(
     },
     firstName: {
       type: String,
-      required: [true, "first name is required"],
     },
     lastName: {
       type: String,
-      required: [true, "last name is required"],
     },
     phone: {
       type: String,
-      required: [true, "phone no is required"],
     },
     email: {
       type: String,
-      required: [true, "email is required"],
     },
     website: {
       type: String,
     },
     address: {
       type: String,
-      required: [true, "address is required"],
     },
     specialization: {
       type: String,
-      required: [true, "specialization is require"],
     },
     experience: {
       type: String,
-      required: [true, "experience is required"],
     },
     feesPerCunsaltation: {
       type: Number,
-      required: [true, "fee is required"],
     },
     status: {
       type: String,
@@ -46,7 +38,6 @@ const mechanicSchema = new mongoose.Schema(
     },
     timings: {
       type: Object,
-      required: [true, "wrok timing is required"],
     },
   },
   { timestamps: true }
