27c2e8d342380d3fc6bbe936b135d44c3cfb94ea
ajith1506 <ajithkumararun1111@gmail.com>
2024-02-27 08:42:58 +0530

    changes

--- models/appoinmentModel.js
+++ models/appoinmentModel.js
@@ -16,20 +16,17 @@ const appointmentSchema = new mongoose.Schema(
     },
     userInfo: {
       type: String,
-      required: true,
     },
     date: {
       type: String,
-      required: true,
     },
     status: {
       type: String,
-      required: true,
+
       default: "pending",
     },
     time: {
       type: String,
-      required: true,
     },
   },
   { timestamps: true }
